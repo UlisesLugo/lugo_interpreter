f4d2813bc99fde3134daee9b3f38c9f16ed73a84
uliseslugo <uliseslf99@hotmail.com>
2022-08-01 17:08:41 -0500

    Parser works for let statements

---
+++ parser/parser.go
@@ -0,0 +1,98 @@
+package parser
+
+import (
+	"fmt"
+
+	"github.com/uliseslugo/lugo_interpreter/ast"
+	"github.com/uliseslugo/lugo_interpreter/lexer"
+	"github.com/uliseslugo/lugo_interpreter/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	currToken token.Token
+	peekToken token.Token
+
+	errors []string
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l, errors: []string{}}
+
+	// Read two tokens, so curToken and PeekToken are both set
+	p.nextToken()
+	p.nextToken()
+	
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.currToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	program := &ast.Program{}
+	program.Statements = []ast.Statement{}
+
+	for p.currToken.Type != token.EOF {
+		stmt := p.parseStatement()
+		if stmt != nil {
+			program.Statements = append(program.Statements, stmt)
+		}
+		p.nextToken()
+	}
+
+	return program
+}
+
+func (p *Parser) parseStatement() ast.Statement {
+	switch p.currToken.Type {
+	case token.LET:
+		return p.parseLetStatement()
+	default:
+		return nil
+	}
+}
+
+func (p *Parser) parseLetStatement() *ast.LetStatement {
+	stmt := &ast.LetStatement{Token: p.currToken}
+
+	if !p.expectPeek(token.IDENTIFIER) {
+		return nil
+	}
+
+	stmt.Name = &ast.Identifier{Token: p.currToken, Value: p.currToken.Literal}
+
+	if !p.expectPeek(token.ASSIGN) {
+		return nil
+	}
+
+	// TODO: We're skipping the expressions until we
+	// encounter a semicolon
+	for token.SEMICOLON != p.currToken.Type {
+		p.nextToken()
+	}
+
+	return stmt
+}
+
+func (p *Parser) expectPeek(t token.TokenType) bool {
+	if p.peekToken.Type == t {
+		p.nextToken()
+		return true
+	} else {
+		return false
+	}
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError (t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
+}
