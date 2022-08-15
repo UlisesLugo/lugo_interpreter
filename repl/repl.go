6c0a8b5b1149704be55d28779ea869fd1cd87cf6
uliseslugo <uliseslf99@hotmail.com>
2022-08-15 18:41:32 -0500

    Added environment to eval

--- repl/repl.go
+++ repl/repl.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/uliseslugo/lugo_interpreter/evaluator"
 	"github.com/uliseslugo/lugo_interpreter/lexer"
+	"github.com/uliseslugo/lugo_interpreter/object"
 	"github.com/uliseslugo/lugo_interpreter/parser"
 )
 
@@ -14,6 +15,7 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Fprint(out, PROMPT)
@@ -32,9 +34,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
-		io.WriteString(out, evaluated.Inspect())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program, env)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
