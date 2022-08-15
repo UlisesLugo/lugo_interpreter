fa19523db2e33b92e1ced7189f6a6274d8896bb1
uliseslugo <uliseslf99@hotmail.com>
2022-08-15 12:22:38 -0500

    Connect parser to repl

--- repl/repl.go
+++ repl/repl.go
@@ -6,7 +6,7 @@ import (
 	"io"
 
 	"github.com/uliseslugo/lugo_interpreter/lexer"
-	"github.com/uliseslugo/lugo_interpreter/token"
+	"github.com/uliseslugo/lugo_interpreter/parser"
 )
 
 const PROMPT = ">> "
@@ -23,10 +23,22 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParseErrors(out, p.Errors())
+			continue
 		}
-	}
 
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParseErrors(out io.Writer, errors []string) {
+	io.WriteString(out, "Woops! We ran into some errors:\n")
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
+	}
 }
